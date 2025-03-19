86a11d963224b8fd6cce848c436d5ecc8fea3048
Melfice <melfice@votion.io>
2025-03-19 11:11:59 +0800

    Include gpio.h in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "spi.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
